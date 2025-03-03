e48a290beb3c31aaa20e18e133d6462c41ab1b2f
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2025-03-03 12:10:34 +0300

    Extract SyncError to file

--- Sources/Scout/Core/Sync.swift
+++ Sources/Scout/Core/Sync.swift
@@ -86,25 +86,3 @@ private func sync(in container: CKContainer) async throws {
         try context.save()
     }
 }
-
-// MARK: - Errors
-
-/// An error type that represents a synchronization error.
-enum SyncError: Error {
-
-    /// The CloudKit container was not found.
-    case containerNotFound
-
-    /// The user is not logged in to iCloud.
-    case notLoggedIn
-
-    var localizedDescription: String {
-        switch self {
-        case .containerNotFound:
-            return
-                "CloudKit container not found. Call `setup(container:)` while initializing the app."
-        case .notLoggedIn:
-            return "User is not logged in to iCloud"
-        }
-    }
-}
