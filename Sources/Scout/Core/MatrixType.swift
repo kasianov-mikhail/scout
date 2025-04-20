60ba7dead29b5c74eb0b9b697603e35878330891
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2025-04-21 02:52:28 +0300

    Add MatrixType protocol and Int extension for matrix record name

---
+++ Sources/Scout/Core/MatrixType.swift
@@ -0,0 +1,19 @@
+//
+// Copyright 2025 Mikhail Kasianov
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+/// A protocol that defines the requirements for a matrix type.
+///
+/// Conforming types must provide a static `recordName` property that specifies the
+/// name of the CloudKit record type used to store the matrix data.
+///
+protocol MatrixType {
+    static var recordName: String { get }
+}
+
+extension Int: MatrixType {
+    static let recordName = "DateIntMatrix"
+}
