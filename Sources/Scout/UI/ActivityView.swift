3568030e924e7bb954c49c174ce493f38a239249
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2025-05-17 22:09:59 +0300

    Implement ActivityView and ChartView; refactor StatView to utilize ChartView for improved chart rendering and data presentation.

---
+++ Sources/Scout/UI/ActivityView.swift
@@ -0,0 +1,40 @@
+//
+// Copyright 2025 Mikhail Kasianov
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import SwiftUI
+import Charts
+
+struct ActivityView: View {
+    @State var model: StatModel<ActivityPeriod>
+    @ObservedObject var activity: ActivityProvider
+
+    init(activity: ActivityProvider, period: ActivityPeriod) {
+        self.activity = activity
+        self._model = State(wrappedValue: StatModel(period: period))
+    }
+
+    var body: some View {
+        VStack(spacing: 0) {
+            PeriodPicker(model: $model, periods: ActivityPeriod.allCases)
+
+            if let points = model.points(from: activity.data) {
+                RangeControl(model: $model)
+                    .padding(.top)
+                    .padding(.horizontal)
+
+                List {
+                    ChartView(points: points, model: model).foregroundStyle(.green)
+                }
+                .listStyle(.plain)
+                .scrollDisabled(true)
+            } else {
+                ProgressView().tint(nil).frame(maxHeight: .infinity)
+            }
+        }
+        .navigationTitle("Active Users")
+    }
+}
