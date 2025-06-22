64b1d6c9f9ced03ad6a105078c7b841881302bf8
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2025-06-22 16:21:02 +0300

    Add custom X axis values for month period in ChartView
    Introduces logic to display specific axis marks for the last 4 weeks when the selected period is a month. This improves axis label accuracy for monthly charts by using custom date values, while other periods retain default axis behavior.

--- Sources/Scout/UI/ChartView.swift
+++ Sources/Scout/UI/ChartView.swift
@@ -19,6 +19,13 @@ struct ChartView<T: ChartCompatible>: View {
                 y: .value("Y", point.count)
             )
         }
+        .chartXAxis {
+            if let axisValues = model.axisValues {
+                AxisMarks(values: axisValues)
+            } else {
+                AxisMarks()
+            }
+        }
         .chartBackground { proxy in
             if points.count == 0 {
                 Placeholder(text: "No results")
@@ -28,5 +35,24 @@ struct ChartView<T: ChartCompatible>: View {
         .padding()
         .padding(.bottom)
         .listRowInsets(EdgeInsets())
+    }
+}
+
+// MARK: - ChartView Extensions
+
+extension StatModel {
+
+    /// Returns the axis values for the chart.
+    ///
+    /// For a month period, the values are the last 4 weeks. This fixes the issue with the axis
+    /// values not being displayed correctly for the month period. For the other periods,
+    /// the chart uses default axis values
+    ///
+    fileprivate var axisValues: [Date]? {
+        if period.rangeComponent == .month {
+            return [-28, -21, -14, -7].map(range.upperBound.addingDay)
+        } else {
+            return nil
+        }
     }
 }
