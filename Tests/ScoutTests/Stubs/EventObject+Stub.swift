65b9729f8cbf0c98b1f9fabd2a66a5e3c3146a44
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2025-08-28 18:58:48 +0300

    Rename Utils and Utilities directories for clarity
    Moved files from 'Utils' to 'Utilities' in source and from 'Utilities' to 'Stubs' in tests to improve directory organization and naming consistency.

--- Tests/ScoutTests/Stubs/EventObject+Stub.swift
+++ Tests/ScoutTests/Stubs/EventObject+Stub.swift
@@ -13,12 +13,20 @@ extension EventObject {
     static func stub(
         name: String,
         date: Date = Date(),
+        isSynced: Bool = false,
         in context: NSManagedObjectContext
     ) -> EventObject {
         let entity = NSEntityDescription.entity(forEntityName: "EventObject", in: context)!
         let event = EventObject(entity: entity, insertInto: context)
+
         event.name = name
         event.date = date
+        event.eventID = UUID()
+        event.isSynced = isSynced
+        event.userID = UUID()
+        event.launchID = UUID()
+        event.sessionID = UUID()
+
         return event
     }
 }
