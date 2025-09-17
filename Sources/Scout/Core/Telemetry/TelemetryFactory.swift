9dc48a7a97fde37e2b4f720ccab97e69d5d890c7
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2025-09-18 02:23:46 +0300

    Refactor TelemetryFactory method signatures
    Simplified method signatures in TelemetryFactory by removing unnecessary line breaks and 'return' statements. This improves code readability and consistency.

--- Sources/Scout/Core/Telemetry/TelemetryFactory.swift
+++ Sources/Scout/Core/Telemetry/TelemetryFactory.swift
@@ -9,40 +9,24 @@ import Foundation
 import Metrics
 
 struct TelemetryFactory: MetricsFactory {
-    func makeCounter(
-        label: String,
-        dimensions: [(String, String)]
-    ) -> CounterHandler {
-        return CKTelemetryHandler(label: label, dimensions: dimensions)
+    func makeCounter(label: String, dimensions: [(String, String)]) -> CounterHandler {
+        CKTelemetryHandler(label: label, dimensions: dimensions)
     }
 
-    func makeFloatingPointCounter(
-        label: String,
-        dimensions: [(String, String)]
-    ) -> FloatingPointCounterHandler {
-        return CKTelemetryHandler(label: label, dimensions: dimensions)
+    func makeFloatingPointCounter(label: String, dimensions: [(String, String)]) -> FloatingPointCounterHandler {
+        CKTelemetryHandler(label: label, dimensions: dimensions)
     }
 
-    func makeMeter(
-        label: String,
-        dimensions: [(String, String)]
-    ) -> MeterHandler {
-        return CKTelemetryHandler.Idle()
+    func makeMeter(label: String,dimensions: [(String, String)]) -> MeterHandler {
+        CKTelemetryHandler.Idle()
     }
 
-    func makeRecorder(
-        label: String,
-        dimensions: [(String, String)],
-        aggregate: Bool
-    ) -> RecorderHandler {
-        return CKTelemetryHandler.Idle()
+    func makeRecorder(label: String,dimensions: [(String, String)],aggregate: Bool) -> RecorderHandler {
+        CKTelemetryHandler.Idle()
     }
 
-    func makeTimer(
-        label: String,
-        dimensions: [(String, String)]
-    ) -> TimerHandler {
-        return CKTelemetryHandler(label: label, dimensions: dimensions)
+    func makeTimer(label: String,dimensions: [(String, String)]) -> TimerHandler {
+        CKTelemetryHandler(label: label, dimensions: dimensions)
     }
 
     func destroyCounter(_ handler: CounterHandler) {}
