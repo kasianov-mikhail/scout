dea7d5db724c1bbec84c6419e2cad3ce14138236
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2026-05-05 18:13:15 +0300

    Set locale to "en_US" for chart date formatting

--- Sources/Scout/UI/Chart/ChartFormat.swift
+++ Sources/Scout/UI/Chart/ChartFormat.swift
@@ -10,6 +10,7 @@ import Foundation
 extension Calendar.Component {
     var chartFormat: Date.FormatStyle {
         var style = Date.FormatStyle()
+        style.locale = Locale(identifier: "en_US")
         style.timeZone = TimeZone(secondsFromGMT: 0)!
 
         return switch self {
