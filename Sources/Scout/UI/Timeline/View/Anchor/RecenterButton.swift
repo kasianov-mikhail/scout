08b89f4780867ca7bd7fc1c48e0206b631fa8754
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2026-06-09 14:09:19 +0300

    Add RecenterButton view and Direction helper

---
+++ Sources/Scout/UI/Timeline/View/Anchor/RecenterButton.swift
@@ -0,0 +1,30 @@
+//
+// Copyright 2026 Mikhail Kasianov
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import SwiftUI
+
+struct RecenterButton: View {
+    let direction: RecenterDirection
+    let action: () -> Void
+
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            let icon = Image(systemName: direction.symbol)
+                .font(.title3)
+                .padding(12)
+            if #available(iOS 26.0, *) {
+                icon.glassEffect(.regular.interactive(), in: Circle())
+            } else {
+                icon.background(Circle().fill(.regularMaterial))
+            }
+        }
+        .padding(20)
+        .transition(.scale.combined(with: .opacity))
+    }
+}
---
+++ Sources/Scout/UI/Timeline/View/Anchor/RecenterDirection.swift
@@ -0,0 +1,32 @@
+//
+// Copyright 2026 Mikhail Kasianov
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import SwiftUI
+
+enum RecenterDirection {
+    case up
+    case down
+
+    init?(frame: CGRect, viewport: CGRect) {
+        if frame.maxY <= viewport.minY {
+            self = .up
+        } else if frame.minY >= viewport.maxY {
+            self = .down
+        } else {
+            return nil
+        }
+    }
+
+    var symbol: String {
+        switch self {
+        case .up: 
+            "chevron.up"
+        case .down:
+            "chevron.down"
+        }
+    }
+}
