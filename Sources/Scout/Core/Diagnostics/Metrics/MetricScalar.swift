8a04ec09ce5ef1b3aca43d29117e77a9e3de7a32
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2026-07-16 20:56:52 +0300

    Collapse the database access protocols into DatabaseReader and DatabaseWriter

--- Sources/Scout/Core/Diagnostics/Metrics/MetricScalar.swift
+++ Sources/Scout/Core/Diagnostics/Metrics/MetricScalar.swift
@@ -9,12 +9,16 @@ import CoreData
 
 protocol MetricScalar: AdditiveArithmetic & Comparable & Hashable & Sendable & RecordValueConvertible {
     associatedtype Object: MetricsValued where Object.Value == Self
+    static var seriesValues: String { get }
+    init(_ value: Double)
 }
 
 extension Int: MetricScalar {
     typealias Object = IntMetricsEntry
+    static var seriesValues: String { "int" }
 }
 
 extension Double: MetricScalar {
     typealias Object = DoubleMetricsEntry
+    static var seriesValues: String { "double" }
 }
